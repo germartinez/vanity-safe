bbbfe8d040e89190b7f82b9ea03fe8c68eacc264
Germán Martínez <germaga92@gmail.com>
2020-11-27 18:01:35 +0100

    Add connect button

---
+++ src/components/ConnectButton/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import Web3Connect from 'web3connect'
+
+const {
+  default: WalletConnectProvider
+} = require('@walletconnect/web3-provider')
+
+const Web3ConnectButton = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+`
+
+type ConnectButtonProps = {
+  onConnect: Function
+}
+
+const ConnectButton = ({ onConnect }: ConnectButtonProps) => (
+  <Web3ConnectButton>
+    <Web3Connect.Button
+      providerOptions={{
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: {
+            infuraId: process.env.REACT_APP_INFURA_ID
+          }
+        }
+      }}
+      onConnect={onConnect}
+      onClose={() => {}}
+    />
+  </Web3ConnectButton>
+)
+
+export default ConnectButton
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
-import avertaFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-normal.woff2'
 import avertaBoldFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-bold.woff2'
+import avertaFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-normal.woff2'
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -23,6 +23,15 @@ const GlobalStyle = createGlobalStyle`
     src: local('Averta'), local('Averta Bold'),
     url(${avertaFont}) format('woff2'),
     url(${avertaBoldFont}) format('woff');
+  }
+
+  .web3connect-connect-button {
+    outline: none;
+    background: #008c73;
+    border: 1px solid #008c73;
+    color: #fff;
+    cursor: pointer;
+    transform: none;
   }
 `
 
