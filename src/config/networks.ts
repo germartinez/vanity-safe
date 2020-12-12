2bc9cc1aff0f69bbbdc19cc45c4362f3eb90069e
Germán Martínez <germaga92@gmail.com>
2020-12-12 02:14:07 +0100

    Add fallback handler to network configuration

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -1,6 +1,7 @@
 export interface NetworkConfigEntry {
   masterCopyAddress: string
   proxyFactoryAddress: string
+  fallbackHandlerAddress: string
 }
 
 export interface NetworksConfig {
@@ -12,35 +13,42 @@ export const defaultNetworks: NetworksConfig = {
   1: {
     masterCopyAddress: '0x6851D6fDFAfD08c0295C392436245E5bc78B0185',
     proxyFactoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
+    fallbackHandlerAddress: '0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44'
   },
   // rinkeby testnet
   4: {
     masterCopyAddress: '0x6851D6fDFAfD08c0295C392436245E5bc78B0185',
     proxyFactoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
+    fallbackHandlerAddress: '0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44'
   },
   // goerli testnet
   5: {
     masterCopyAddress: '0x6851D6fDFAfD08c0295C392436245E5bc78B0185',
     proxyFactoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
+    fallbackHandlerAddress: '0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44'
   },
   // kovan testnet
   42: {
     masterCopyAddress: '0x6851D6fDFAfD08c0295C392436245E5bc78B0185',
     proxyFactoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
+    fallbackHandlerAddress: '0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44'
   },
   // xdai
   100: {
     masterCopyAddress: '0x6851D6fDFAfD08c0295C392436245E5bc78B0185',
     proxyFactoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
+    fallbackHandlerAddress: '0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44'
   },
   // energy web chain
   246: {
     masterCopyAddress: '0x6851D6fDFAfD08c0295C392436245E5bc78B0185',
     proxyFactoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
+    fallbackHandlerAddress: '0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44'
   },
   // energy web chain volta testnet
   73799: {
     masterCopyAddress: '0x6851D6fDFAfD08c0295C392436245E5bc78B0185',
     proxyFactoryAddress: '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
+    fallbackHandlerAddress: '0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44'
   }
 }
